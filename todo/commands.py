8e72ababdaed10fd626e9a8b5179a674aec4bc83
0dihyun <00dihyun@gmail.com>
2025-05-20 23:48:12 +0900

    feat: add done function

--- todo/commands.py
+++ todo/commands.py
@@ -1,19 +1,82 @@
-todo_list = []
+import os
+import sys
+import json
+import traceback
+
+STORAGE_FILE = "todo/storage.json"
+
+def load_todo():
+    if os.path.exists(STORAGE_FILE):
+        with open(STORAGE_FILE, "r", encoding="utf-8") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+    else:
+        return []
+
+def save_todo(todo):
+    with open(STORAGE_FILE, "w", encoding="utf-8") as f:
+        json.dump(todo, f, ensure_ascii=False, indent=2)
+
+
+def greeting():
+    print("Hi! Nice to meet you!")
+
+def help():
+    print("""hi      | 인사를 합니다
+ls      | todo 리스트를 보여줍니다
+add     | todo를 추가합니다
+del     | todo를 삭제합니다
+del -a  | 모든 todo를 삭제합니다
+exit    | 프로그램을 종료합니다""")
 
 def ls():
-    if not todo_list:
-        print("없습니다")
+    todos = load_todo()
+    if len(todos):
+        print("{:<10}{:<10}{:<10}".format("number", "status", "task"))
+        print("---------------------------")
+        for idx, todo in enumerate(todos, start=1):
+            status = "O" if todo.get("done") else "X"
+            print("{:<10}{:<10}{:<10}".format(idx, status, todo.get('task')))
     else:
-        for i, task in enumerate(todo_list, start=1):
-            print(f"{i}. {task}")
+        print("[!] todo가 없습니다")
 
 def add(todo):
-    todo_list.append(todo)
-    print("todo를 추가했습니다")
+    try:
+        todos = load_todo()
+        todos.append({"task": todo, "done": False})
+        save_todo(todos)
+        print("[+] todo를 추가했습니다")
+    except Exception as e:
+        print("[!] 오류발생", e)
+        traceback.print_exc()    
 
-def delete(todo:int):
-    try: 
-        todo_list.pop(todo - 1)
-        print("todo를 삭제하였습니다")
-    except IndexError:
-        print("다시 입력해 주세요")
+def delete(todo_number:int, all:bool=False):
+    todos = load_todo()
+    if all:
+        try:
+            todos.clear()
+            save_todo(todos)
+            print("[-] 모든 todo를 삭제하였습니다")
+        except Exception as e:
+            print("[!] 오류발생", e)
+    else:
+        try:
+            todos.pop(todo_number - 1)
+            save_todo(todos)
+            print("[-] todo를 삭제하였습니다")
+        except Exception as e:
+            print("[!] 오류발생", e)
+
+def done(todo_number):
+    try:
+        todos = load_todo()
+        todos[todo_number - 1]["done"] = True
+        save_todo(todos)
+        print("[+] 상태를 변경하였습니다")
+    except Exception as e:
+        print("[!] 오류발생", e)
+
+def exit():
+    sys.exit()
--- todo/main.py
+++ todo/main.py
@@ -1,8 +1,13 @@
-from todo.commands import ls, add, delete    
+from todo.commands import greeting, ls, add, delete, exit, help, done
+
 commands = {
+    "hi": greeting, 
     "ls": ls,
     "add": add,
     "del": delete,
+    "exit": exit,
+    "help": help,
+    "done": done,
 }
 
 def main():
@@ -11,8 +16,13 @@ def main():
         if user_input.startswith("add "):
             add(user_input[4:])
         elif user_input.startswith("del "):
-            delete(int(user_input[4:]))
+            if user_input[4:] == "-a":
+                delete(todo_number=0, all=True)
+            else:
+                delete(int(user_input[4:]))
+        elif user_input.startswith("done "):
+            done(int(user_input[4:]))
         elif user_input in commands:
             commands[user_input]()
         else:
-            print("명령어를 찾을 수 없습니다.")
+            print("[!] 명령어를 찾을 수 없습니다.")
